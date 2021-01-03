d7d1a272a0a2053857440132ef2d0b14bdb50fac
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-03 23:19:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,15 @@
 def create_an_empty_array
+  []
   
 end
 
-def create_an_array
+def create_an_array 
+  ["Donovan", "Drake", "Jr", "Patty"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+  create_an_array << |"Morgan", "Dad"|
   
 end
 
